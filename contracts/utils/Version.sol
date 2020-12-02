853e0793daca107134e26c0806281a8e043a4a7e
Jiri Malek <jirka.malek@gmail.com>
2020-12-02 15:17:31 +0100

    Add Bridge contracts version tracking.

---
+++ contracts/utils/Version.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.0;
+
+// Version implements the current version getter.
+contract Version {
+    // version reports the current version of the contract
+    function version() {
+        // version 0.1.0
+        return "010";
+    }
+}
