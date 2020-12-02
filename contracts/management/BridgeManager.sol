efac9617e3c83590ee2ebcfb54373a44c7bbed3a
Jiri Malek <jirka.malek@gmail.com>
2020-12-02 15:27:08 +0100

    Add support for ERC20 tokens staking.

--- contracts/management/BridgeManager.sol
+++ contracts/management/BridgeManager.sol
@@ -2,27 +2,37 @@
 pragma solidity ^0.6.0;
 
 import "../common/SafeMath.sol";
+import "../common/IERC20.sol";
+import "../common/SafeERC20.sol";
 import "../ownership/Ownable.sol";
+import "../utils/Version.sol";
 import "./ManagerConstants.sol";
 
 // BridgeManager implements the Fantom CrossChain Bridge management
 // contract.
-contract BridgeManager is Initializable, Ownable, ManagerConstants {
+contract BridgeManager is Initializable, Ownable, ManagerConstants, Version {
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
     // Validator represents a Bridge validator record
     struct Validator {
         // status pf the Validator
         uint256 status;
+        uint256 receivedStake;
 
         // milestones in the Validator's life
         uint256 createdTime;
-        uint256 createdEpoch;
         uint256 deactivatedTime;
-        uint256 deactivatedEpoch;
 
         // authorized address of the Validator
         address auth;
+    }
+
+    // UnstakingRequest represents a structure describing
+    // a request for (partial) stake removal.
+    struct UnstakingRequest {
+        uint256 time;
+        uint256 amount;
     }
 
     // getValidator represents the list of validators by their IDs
@@ -31,6 +41,18 @@ contract BridgeManager is Initializable, Ownable, ManagerConstants {
     // getValidatorID represents the relation between a Validator address
     // and the ID of the Validator.
     mapping(address => uint256) public getValidatorID;
+
+    // getDelegation represents the amount of stake granted by a staker address
+    // to the given validator.
+    // Map: Staker => ValidatorID => Amount
+    mapping(address => mapping(uint256 => uint256)) public getDelegation;
+
+    // getUnstakingRequest represents the structure of un-staking requests
+    // being placed from the given staker address to the given validator.
+    // Each request must have a unique requestID identifier under which
+    // the unstaking is registered.
+    // Map: Staker => ValidatorID => RequestID => UnstakingRequest struct
+    mapping(address => mapping(uint256 => mapping(uint256 => UnstakingRequest))) public getUnstakingRequest;
 
     // lastValidatorID represents the ID of the latest validator;
     // it also represents the total number of Validators since a new validator
@@ -41,18 +63,96 @@ contract BridgeManager is Initializable, Ownable, ManagerConstants {
     // all active Validators.
     uint256 public totalStake;
 
+    // stakingToken represents the address of an ERC20 token used for staking
+    // on the Bridge.
+    IERC20 public stakingToken;
+
     // validatorMetadata represents the metadata of a Validator by the ID;
     // please check documentation for the expected metadata structure (JSON object)
     mapping(uint256 => bytes) public validatorMetadata;
 
-    // initialize sets the initial state of the bridge manager
-    function initialize(address[] memory genesisValidators) external initializer {
+    // initialize sets the initial state of the bridge manager creating the genesis set
+    // of bridge validators and assigning the contract owner and staking token
+    function initialize(address sToken, address[] memory gnsValidators) external initializer {
         // initialize the owner
         Ownable.initialize(msg.sender);
 
+        // setup the token
+        stakingToken = IERC20(sToken);
+
         // create the first genesis set of validators
-        for (uint256 i = 0; i < genesisValidators.length; i++) {
-            _createValidator(genesisValidators[i]);
+        // we don't handle staking here, the stake
+        // for this genesis batch will be added later through stake() call
+        for (uint256 i = 0; i < gnsValidators.length; i++) {
+            _createValidator(gnsValidators[i]);
         }
+    }
+
+    // createValidator allows a new candidate to sign up as a validator
+    function createValidator(uint256 stake) external {
+        // make the validator record
+        _createValidator(msg.sender);
+
+        // register the validator stake
+        _stake(_sender(), lastValidatorID, stake);
+    }
+
+    // stake increases the stake of the given validator identified
+    // by the validator ID from the sender by the given amount of staking tokens.
+    function stake(uint256 validatorID, uint256 amount) external {
+        // validator must exist to receive stake
+        require(getValidator[validatorID].status > 0, "BridgeManager: unknown validator");
+
+        // process the stake
+        _stake(_sender(), validatorID, amount);
+    }
+
+    // _createValidator builds up the validator structure
+    function _createValidator(address auth) internal {
+        // make sure the validator does not exist yet
+        require(getValidatorID[auth] == 0, "BridgeManager: validator already exists");
+
+        // what will be the new validator id
+        uint256 validatorID = ++lastValidatorID;
+        getValidatorID[auth] = validatorID;
+
+        // update the validator core record
+        getValidator[validatorID].status = STATUS_NEW;
+        getValidator[validatorID].createdTime = _now();
+        getValidator[validatorID].auth = auth;
+    }
+
+    // _stake processes a new stake of a validator
+    function _stake(address sender, uint256 toValidator, uint256 amount) internal {
+        // make sure the validator is not inactive
+        require(getValidator[toValidator].status & MASK_INACTIVE == 0, "BridgeManager: validator not active");
+
+        // verify we can pull the stake tokens from the staker
+        require(amount <= stakingToken.allowance(sender, address(this)), "BridgeManager: allowance too low");
+
+        // transfer the stake tokens first
+        stakingToken.safeTransferFrom(sender, address(this), amount);
+
+        // remember the stake and add the staked amount to the validator's total stake value
+        getDelegation[sender][toValidator] = getDelegation[sender][toValidator].add(amount);
+        getValidator[toValidator].receivedStake = getValidator[toValidator].receivedStake.add(amount);
+
+        // make sure the validator stake is at least the min amount required
+        require(minStakeAmount() <= getValidator[toValidator].receivedStake, "BridgeManager: stake too low");
+    }
+
+    // _isSelfStake checks if the given stake is actually an initial stake of the validator.
+    function _isSelfStake(address delegator, uint256 toValidatorID) internal view returns (bool) {
+        return getValidatorID[delegator] == toValidatorID;
+    }
+
+    // _sender returns the address of the current trx sender
+    function _sender() internal view returns (address) {
+        return msg.sender;
+    }
+
+    // _now returns the current timestamp as available to the contract call
+    function _now() internal view returns (uint256) {
+        return block.timestamp;
     }
 }
--- contracts/management/ManagerConstants.sol
+++ contracts/management/ManagerConstants.sol
@@ -1,16 +1,32 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
 // ManagerConstants implements constants used in the BridgeManager
 // to control certain aspects of its functionality.
 contract ManagerConstants {
     // STATUS constants represent the binary encoded states validators can gain
-    uint256 internal constant STATUS_NEW = 0;
-    uint256 internal constant STATUS_SYNCED = 1;
-    uint256 internal constant STATUS_WITHDRAWN = 1 << 1;
+    uint256 internal constant STATUS_CREATED = 1;
+    uint256 internal constant STATUS_SYNCED = 1 << 1;
+    uint256 internal constant STATUS_WITHDRAWN = 1 << 2;
     uint256 internal constant STATUS_OFFLINE = 1 << 3;
-    uint256 internal constant STATUS_KICKED = 1 << 4;
-    uint256 internal constant STATUS_ERROR = 1 << 7;
+    uint256 internal constant STATUS_ERROR = 1 << 4;
 
-    // MASK_JAILED is used to recognize if a validator is in jail
-    uint256 internal constant MASK_JAILED = STATUS_ERROR;
+    // MASK_INACTIVE is used to recognize if a validator is active
+    uint256 internal constant MASK_INACTIVE = STATUS_WITHDRAWN | STATUS_OFFLINE | STATUS_ERROR;
+
+    // minStakeAmount returns minimal amount of stake tokens expected to be staked
+    // by a new validator to be accepted.
+    function minStakeAmount() public pure returns (uint256) {
+        // 3.175.000,00 sFTM
+        return 3175000 * 1e18;
+    }
+
+    // unstakePeriodTime returns the number of seconds between an unstake request
+    // and the actual stake unlock time.
+    // If a staker requests to withdraw, the withdraw amount is placed into a request
+    // and after the unstakePeriodTime() seconds pass, they can ask the contract
+    // to send them the withdrawn amount.
+    function unstakePeriodTime() public pure returns (uint256) {
+        // 7 days
+        return 60 * 60 * 24 * 7;
+    }
 }
